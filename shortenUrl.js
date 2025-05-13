f5f454b21de8206bd453281e33ec4a09b71586f4
gooober444 <rich.oil3091@fastmail.com>
2025-05-13 23:31:14 +0200

    Create `shortenUrl.js`

---
+++ shortenUrl.js
@@ -0,0 +1,7 @@
+javascript:(function() {
+  const sourceEncodedUrl = encodeURIComponent(window.location.href)
+  fetch(`https://tinyurl.com/api-create.php?url=${sourceEncodedUrl}`)
+    .then(response => response.text())
+    .then(shortUrl => prompt(`Shortened URL: ${shortUrl}`))
+    .catch(error => alert(`Error shortening URL: ${error}`));
+} )();
