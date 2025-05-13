2a0a3d9e9c81e87cf6d4477415d1303cf2452c06
gooober444 <rich.oil3091@fastmail.com>
2025-05-14 01:05:14 +0200

    Create `force1PasswordMaskedmail.js`

---
+++ force1PasswordMaskedmail.js
@@ -0,0 +1,21 @@
+(function forceEmailForm() {
+    // This will spawn a form that will trigger 1Password's ability to generate a masked e-mail for the domain
+    const html = `
+        <form>
+            <label for="email" style="display: none;">Email</label>
+            <input 
+                id="op-masked-email-helper" 
+                type="email" 
+                name="email" 
+                autocomplete="email"
+            >
+            <button type="submit">Submit</button>
+        </form>
+    `;
+    
+    // Insert the form at the beginning of the document body
+    document.body.insertAdjacentHTML('afterbegin', html);
+    
+    // Focus on the email input field
+    document.getElementById('op-masked-email-helper').focus();
+} )();
