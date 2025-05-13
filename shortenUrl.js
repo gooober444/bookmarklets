94ef1ff68e2c1a04a7bff7b4d171a2502934474e
gooober444 <rich.oil3091@fastmail.com>
2025-05-14 00:16:57 +0200

    `shortenUrl.js`: add newline to aid copy-pasting

--- shortenUrl.js
+++ shortenUrl.js
@@ -1,7 +1,8 @@
 javascript:(function() {
   const sourceEncodedUrl = encodeURIComponent(window.location.href)
+  // Errors out on Reddit's redesigned frontend, super weird
   fetch(`https://tinyurl.com/api-create.php?url=${sourceEncodedUrl}`)
     .then(response => response.text())
-    .then(shortUrl => alert(`Shortened URL: ${shortUrl}`))
+    .then(shortUrl => alert(`Shortened URL:\n ${shortUrl}`))
     .catch(error => alert(`Error shortening URL: ${error}`));
 } )();
