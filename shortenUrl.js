64dd23e1dfed418308b6c8f6da0da90b64b7beb3
gooober444 <rich.oil3091@fastmail.com>
2025-05-14 00:06:56 +0200

    `shortenUrl.js`: change 'prompt' to 'alert' because I'm a goober

--- shortenUrl.js
+++ shortenUrl.js
@@ -2,6 +2,6 @@ javascript:(function() {
   const sourceEncodedUrl = encodeURIComponent(window.location.href)
   fetch(`https://tinyurl.com/api-create.php?url=${sourceEncodedUrl}`)
     .then(response => response.text())
-    .then(shortUrl => prompt(`Shortened URL: ${shortUrl}`))
+    .then(shortUrl => alert(`Shortened URL: ${shortUrl}`))
     .catch(error => alert(`Error shortening URL: ${error}`));
 } )();
